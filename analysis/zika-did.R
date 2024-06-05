1635e3a82b14674e11e4ce2e6268984983ce6507
Lee Kennedy-Shaffer <lkennedyshaffer@vassar.edu>
2024-06-05 15:46:27 -0400

    RS: DID analysis handout

--- analysis/zika-did.R
+++ analysis/zika-did.R
@@ -6,24 +6,24 @@
 ## See Taddeo et al. 2022 (https://doi.org/10.4310/21-SII704) for context and
 ## https://doi.org/10.7910/DVN/ENG0IY for source data.
 
+## Load data:
+load("data/zika.Rda")
+
 ## Libraries:
 require(tidyverse)
 require(lme4)
 
-### load data:
-load("data/zika.Rda")
-
 ### Plot trends (cf. Fig. 2(a) and Fig. 2(b) in Taddeo)
-ggplot(data=zika_summ, mapping=aes(x=Year, y=Rate, color=State, linetype=State)) +
-  geom_line() +
+ggplot(data=zika_summ) +
+  geom_line(mapping=aes(x=Year, y=Rate, color=State, linetype=State)) +
   theme_bw() +
   scale_y_continuous(name="Birth Rate (per 1000)",
                      limits=c(0,17),
                      breaks=seq(0,16,by=4),
                      minor_breaks=seq(0,16,by=2)) +
   geom_vline(xintercept=2015, linetype="longdash", color="grey50")
-ggplot(data=zika_summ, mapping=aes(x=Year, y=LogBirths, color=State, linetype=State)) +
-  geom_line() +
+ggplot(data=zika_summ) +
+  geom_line(mapping=aes(x=Year, y=LogBirths, color=State, linetype=State)) +
   theme_bw() +
   scale_y_continuous(name="Log number of live births",
                      limits=c(10,12),
@@ -42,13 +42,13 @@ ggplot(data=zika_summ, mapping=aes(x=Year, y=LogRate, color=State, linetype=Stat
   geom_vline(xintercept=2015, linetype="longdash", color="grey50")
 
 ### Linear models
-zika_lm <- lm(Rate~trt+year+interaction, data=zika_full)
+zika_lm <- lm(Rate~State+year+interaction, data=zika_full)
 summary(zika_lm)
-confint(zika_lm, level=0.95)
+confint(zika_lm, level=0.95)["interaction",]
 ### Mixed effects model for clustered CI:
-zika_lmer <- lmer(Rate~trt+year+interaction+(1|Code), data=zika_full)
+zika_lmer <- lmer(Rate~State+year+interaction+(1|Code), data=zika_full)
 summary(zika_lmer)
-confint(zika_lmer, level=0.95)
+confint(zika_lmer, level=0.95)["interaction",]
 ### Block-bootstrap for CI:
 codes <- zika_full %>% dplyr::select(Code,State) %>% distinct() # get unique Codes by State
 boot_data <- function() slice_sample(codes, by=State, prop=1, replace=TRUE) %>%
@@ -56,37 +56,61 @@ boot_data <- function() slice_sample(codes, by=State, prop=1, replace=TRUE) %>%
   left_join(zika_full, by = join_by(Code, State, StudyYear))
 set.seed(3671)
 lm_boot <- replicate(n=1000,
-                     expr=lm(Rate~trt+year+interaction,
+                     expr=lm(Rate~State+year+interaction,
                              data=boot_data())$coefficients["interaction"])
 quantile(lm_boot, probs=c(0.5,0.025,0.975))
 
+
+### Placebo in-time
+# Conduct the same analysis for each pair of years on the state level and plot the estimates
+zika_plac <- tibble(`Treated Year`=unique(zika_summ$Year),
+                    `Untreated Year`=`Treated Year`-2) %>%
+  dplyr::filter(`Untreated Year` %in% unique(zika_summ$Year)) %>%
+  left_join(zika_summ %>% dplyr::select(Year,State,Rate) %>% 
+              pivot_wider(id_cols=Year, names_from=State, values_from=Rate),
+            by=join_by(`Treated Year`==Year)) %>% 
+  rename(P1=Pernambuco, R1=`Rio Grande do Sul`) %>%
+  left_join(zika_summ %>% dplyr::select(Year,State,Rate) %>% 
+              pivot_wider(id_cols=Year, names_from=State, values_from=Rate),
+            by=join_by(`Untreated Year`==Year)) %>% 
+  rename(P0=Pernambuco, R0=`Rio Grande do Sul`) %>%
+  mutate(Estimate=(P1-P0)-(R1-R0))
+
+ggplot(data=zika_plac) +
+  geom_point(mapping=aes(x=`Treated Year`, y=Estimate),
+             size=2) +
+  theme_bw() +
+  labs(x="Treated (Placebo) Year", y="DID Estimate") +
+  scale_y_continuous(limits=c(-2,0), breaks=seq(-2,0,by=.5))
+            
+
 ### Log-scale model
-zika_log_lm <- lm(LogRate~trt+year+interaction, data=zika_full)
+zika_log_lm <- lm(LogRate~State+year+interaction, data=zika_full)
 summary(zika_log_lm)
 confint(zika_log_lm, level=0.95)
 ### Mixed effects model for clustered CI:
-zika_log_lmer <- lmer(LogRate~trt+year+interaction+(1|Code), data=zika_full)
+zika_log_lmer <- lmer(LogRate~State+year+interaction+(1|Code), data=zika_full)
 summary(zika_log_lmer)
 confint(zika_log_lmer, level=0.95)
 ### Block-bootstrap for CI:
 set.seed(98391)
 log_boot <- replicate(n=1000,
-                      expr=lm(LogRate~trt+year+interaction,
+                      expr=lm(LogRate~State+year+interaction,
                               data=boot_data())$coefficients["interaction"])
 quantile(log_boot, probs=c(0.5,0.025,0.975))
 
 ### Poisson glm model
-zika_glm <- glm(Births~trt+year+interaction+offset(log(Pop)), data=zika_full,
+zika_glm <- glm(Births~State+year+interaction+offset(log(Pop)), data=zika_full,
                 family=poisson(link="log"))
 summary(zika_glm)
 confint(zika_glm, level=0.95)
-zika_glmer <- glmer(Births~trt+year+interaction+offset(log(Pop))+(1|Code), 
+zika_glmer <- glmer(Births~State+year+interaction+offset(log(Pop))+(1|Code), 
                     data=zika_full,
                     family=poisson(link="log"))
 summary(zika_glmer)
 confint(zika_glmer, level=0.95)
 set.seed(4008335)
 glm_boot <- replicate(n=1000,
-                      expr=glm(Births~trt+year+interaction+offset(log(Pop)), data=boot_data(),
+                      expr=glm(Births~State+year+interaction+offset(log(Pop)), data=boot_data(),
                                family=poisson(link="log"))$coefficients["interaction"])
 quantile(glm_boot, probs=c(0.5,0.025,0.975))
